1b0a19473be1cf4639f7f4c8f316ea5c4381c7bf
shihaotan91 <shihaotan91@gmail.com>
2019-03-31 22:41:20 +0800

    WIP - create gameboard module with answer validations method

---
+++ app/services/game_board/check_answer.rb
@@ -0,0 +1,44 @@
+module GameBoard
+  class CheckAnswer
+    def initialize(game, result, answer)
+      @game = game
+      @result = result
+      @answer = answer
+      @response = { errors: [] }
+      @dictionary_details = Rails.cache.fetch(Game::DICTIONARY_DETAILS)
+      @position_neighbours = I18n.t('games.board_position_neighbour')
+
+      validate_answer
+    end
+
+    def validate_answer
+      check_for_invalid_characters
+      check_answer_length
+      check_duplicated_answers
+
+      return @response[:errors] unless @response[:errors].empty?
+    end
+
+    def check_for_invalid_characters
+      alphabet_regex = /^[a-zA-Z]+$/
+      unless @answer.match(alphabet_regex)
+        @response[:errors] << 'Your answer must contain only letters'
+      end
+    end
+
+    def check_answer_length
+      min_length = @dictionary_details[:min_length]
+      max_length = @dictionary_details[:max_length]
+      unless @answer.length.between?(min_length, max_length)
+        @response[:errors] << "Word length must be between #{min_length} and #{max_length}"
+      end
+    end
+
+    def check_duplicated_answers
+      all_answers = @result.correct_answers + @result.wrong_answers
+      if all_answers.include? @answer
+        @response[:errors] << "You've answered with this word(#{@answer} before"
+      end
+    end
+  end
+end
