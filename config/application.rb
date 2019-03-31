aa1888deae5256f1173984a7eafcff53d432c91b
shihaotan91 <shihaotan91@gmail.com>
2019-03-31 20:58:40 +0800

    WIP - add dictionary to en yml

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module BoggleAssignment
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
   end
 end
