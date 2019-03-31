a95061a1bae740e69aea5a5234cb9a3b07723548
shihaotan91 <shihaotan91@gmail.com>
2019-03-31 17:11:37 +0800

    WIP - add generate token method to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,19 @@
 class Game < ApplicationRecord
   after_create :generate_board
+  after_create :generate_token
+
+  validates :token, uniqueness: true
 
   def generate_board
     possible_letters = ('A'..'Z').to_a
     wildcards = ['*', '*']
     letters_board = (0..13).map { possible_letters[rand(26)] } + wildcards
     self.board = letters_board.shuffle
+    save
+  end
+
+  def generate_token
+    self.token = SecureRandom.hex(20)
     save
   end
 end
