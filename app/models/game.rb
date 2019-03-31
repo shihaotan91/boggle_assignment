31052bf77097ffd0b3cc203435230c66a2116b4d
shihaotan91 <shihaotan91@gmail.com>
2019-03-31 17:11:06 +0800

    WIP - add generate board method to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,11 @@
 class Game < ApplicationRecord
+  after_create :generate_board
+
+  def generate_board
+    possible_letters = ('A'..'Z').to_a
+    wildcards = ['*', '*']
+    letters_board = (0..13).map { possible_letters[rand(26)] } + wildcards
+    self.board = letters_board.shuffle
+    save
+  end
 end
