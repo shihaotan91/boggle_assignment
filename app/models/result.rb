97d624369ddde68f711350339cb7ead5b6fd0656
shihaotan91 <shihaotan91@gmail.com>
2019-04-04 22:05:12 +0800

    WIP - add method to calculate points in result model

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,10 @@
 class Result < ApplicationRecord
   belongs_to :game, dependent: :destroy
+  before_update :calculate_points
+
+  def calculate_points
+    return if correct_answers.empty?
+
+    self.points = correct_answers.map(&:length).sum
+  end
 end
